01fd13b1c5cc8a0420c8d0f6af72c6724aa0b643
Caroline Cripa <146898564+CarolCripa@users.noreply.github.com>
2025-04-17 00:32:53 +0000

    codificar exercicio 1

---
+++ main.js
@@ -0,0 +1,13 @@
+let dados = [97, 45, 49, 32, 2, 5, 9, 99, 1, 7];
+// percorrer todos os itens do array
+for (let i = 0; i < dados.length; i++) {
+  console.log ("o indice é:" + i + "o valor é:" + dados [i]);
+}
+
+// para cada item, verificar se ele é maior que o próximo
+
+// se for inverter ambos os valores
+
+// caso contrário, não fazer nada e avançar para o próximo
+
+console.log(dados);
